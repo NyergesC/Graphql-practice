b4c06fce79748f41f1be93c3b0ec8b6fdfcfb74e
NyergesC <cintia.nyerges@gmail.com>
2022-09-09 12:56:53 +0200

    array of objects type added

--- index.js
+++ index.js
@@ -2,11 +2,29 @@
 
 const { ApolloServer, gql } = require('apollo-server')
 
-//SCHEMA: to define what type of data I want for example from the fetch: (how our queries going to look): String, 
+//SCHEMA: to define what type of data I want for example from the fetch: (how our queries going to look)
+//Scalar types: String, Int, Float, Boolean , Array (Arrayen beluli tipusok is lehetnek null-ok, !-el tudom fixalni: [String!] v [String!]!)
+//null is one exception!! all the types can be nothing = null as well or we can use ! to avoid that flexibility
+
+//define how our product (object) going to look: type Product tehat object eseten alatta keszitsuk el a semat hogy fog az object kinezni
 
 const typeDefs = gql` 
     type Query {
-        hello: String
+        hello: String!
+        numberOfAnimals: Int
+        price: Float
+        isCool: Boolean
+        helloTwo: [String!]!
+
+        products: [Product!]!
+    }
+
+    type Product {
+        name: String!
+        description: String!
+        quantity: Int!
+        price: Float!
+        onSale: Boolean!
     }
 `
 
@@ -15,8 +33,29 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         hello: () => {
-            return "World"
-        }
+            return "World!!!"
+        },
+        numberOfAnimals: () => {
+            return 55
+        },  
+        price: () => {
+            return 123.456
+        },   
+        isCool: () => {
+            return true
+        },
+        helloTwo: () => {
+            return ['hello', 'my', 'friend']
+        } ,   
+        products: () => {
+            return [{
+                name: "Darth",
+                description: "valami",
+                quantity: 4,
+                price: 1234,
+                onSale: true,
+            }]
+        }    
     }
 }
 
